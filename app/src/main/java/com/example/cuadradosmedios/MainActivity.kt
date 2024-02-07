ad673936c7529d9bfc0430795d708c3213c40946
Guerrero-SDFX <you@example.com>
2024-02-06 19:02:50 -0600

    Update | Ya no se bloquea la app si se pasa de 0000

--- app/src/main/java/com/example/cuadradosmedios/MainActivity.kt
+++ app/src/main/java/com/example/cuadradosmedios/MainActivity.kt
@@ -11,15 +11,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    //Los arreglos que se usarán para hacer las iteraciones
     private lateinit var x: Array<String>
     private lateinit var y: Array<String>
     private lateinit var r: Array<String>
+
+    private var indice = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        //Variables para mostrar la lista
         val recyclerView: RecyclerView = findViewById(R.id.rv_nR)
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
@@ -28,8 +33,9 @@ class MainActivity : AppCompatActivity() {
         val adaptador = MiAdaptador(listaDeDatos)
         recyclerView.adapter = adaptador
 
+        //Aqui se obtiene el botón generar
         binding.btnGenerar.setOnClickListener {
-            listaDeDatos.clear()
+            listaDeDatos.clear() //Se limpia la lista de datos
             var datosValidos = true
             val seed = binding.txtInputSeed.text.toString()
             val iteraciones = binding.txtInputNR.text.toString()
@@ -37,7 +43,7 @@ class MainActivity : AppCompatActivity() {
             binding.tiSeed.error = null
             binding.tiNR.error = null
 
-            if (seed.length < 3) {
+            if (seed.length < 4) {
                 binding.tiSeed.error = "Ingrese más de 3 dígitos"
                 datosValidos = false
             }
@@ -47,25 +53,25 @@ class MainActivity : AppCompatActivity() {
                 datosValidos = false
             }
 
-            if (datosValidos) {
-                val d = seed.length
+            if (datosValidos) { //Si los datos son validos, realiza el procedimiento
+                val d = seed.length //Se obtiene la semilla que de el usuario
                 val nI = iteraciones.toIntOrNull() ?: 0
+                //Se inicializan los arreglos
                 x = Array(nI+1) { "" }
                 y = Array(nI+2) { "" }
                 r = Array(nI+2) { "" }
-
-
+                //Se le da el valor a x[0] y r[0]
                 x[0] = seed
                 r[0] = " "
 
+                generarR(d, nI) //se llama a una función que hace el procedimiento
 
-                generarR(d, nI)
-
-                for(i in 0 until nI){
+                //Despues del procedimiento, simplemente se muestran los datos en una cadena
+                for(i in 0 until indice+1){
                     listaDeDatos.add("Y$i=${y[i]} X${i+1}=${x[i+1]} R${i+1}=${r[i+1]}")
                 }
                 adaptador.notifyDataSetChanged()
-
+                //Se muestra un mensaje que indica que la generación se ha completado
                 Toast.makeText(this, "Generación completada", Toast.LENGTH_SHORT).show()
             }
         }
@@ -73,32 +79,34 @@ class MainActivity : AppCompatActivity() {
 
     private fun generarR(d: Int, nI: Int) {
         for (i in 0 until nI) {
-            val xInt = x[i].toLong()
-            val yTemp: Long =xInt*xInt
-            if(yTemp > 0){
+            if(x[i] != "0000"){ //Verifica que la semilla no sea puro 0000
+                val xInt = x[i].toLong()
+                val yTemp: Long =xInt*xInt
                 y[i] = yTemp.toString()
-                var dY = y[i].length
+                var dY = y[i].length //Con esto se obtiene la longitud de Yi
 
-                if (d % 2 == 0) {
-                    if (dY % 2 != 0) {
-                        y[i] = "0" + y[i]
+                if (d % 2 == 0) { //Si la cantidad de digitos de la semilla es par
+                    if (dY % 2 != 0) { //y la cantidad de digitos de Yi es impar
+                        y[i] = "0" + y[i] //Se le añade un 0 a la izquierda
                     }
-                } else {
-                    if (dY % 2 == 0) {
-                        y[i] = "0" + y[i]
+                } else { //Si la cantidad de digitos de la semilla es impar
+                    if (dY % 2 == 0) { //Y la cantidad de digitos de Yi es par
+                        y[i] = "0" + y[i] //Se le añade un 0 a la izquierda
                     }
                 }
 
                 dY = y[i].length
-                val index = (dY - d) / 2
-                val xTemp = y[i].substring( index , index + d)
+                val index = (dY - d) / 2 //Con esto se encuentra la cantidad sobrante de los extremos
+                val xTemp = y[i].substring( index , index + d) //Y se corta la parte de en medio de la cadena
                 var j = i+1
                 if (j < nI+1) { // Verifica que j sea menor que nI
                     x[j] = xTemp
-                    r[j] = "0.$xTemp"
+                    r[j] = "0.$xTemp" //se le añade un 0. al numero
+                    indice = i
                 }
             } else {
-                Toast.makeText(this, "Seed fuera de rango", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Fuera de rango", Toast.LENGTH_SHORT).show() //Si la semilla contiene 0, se indica que supero el rango
+                break
             }
 
         }
